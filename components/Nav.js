180f839fae72acd02bd50a3876969dda3f4c6bf6
Christopher Arsenault <christopher.arsenault82@gmail.com>
2020-02-10 21:48:04 -0500

    initial commit

---
+++ components/Nav.js
@@ -0,0 +1,32 @@
+const Nav = () => {
+	return (
+		<nav className='navbar navbar-expand-lg navbar-dark bg-primary'>
+			<div className='collapse navbar-collapse' id='navbarColor01'>
+				<ul className='navbar-nav mr-auto'>
+					<li className='nav-item active'>
+						<a className='nav-link' href='#'>
+							Home <span className='sr-only'>(current)</span>
+						</a>
+					</li>
+					<li className='nav-item'>
+						<a className='nav-link' href='#'>
+							Features
+						</a>
+					</li>
+					<li className='nav-item'>
+						<a className='nav-link' href='#'>
+							Pricing
+						</a>
+					</li>
+					<li className='nav-item'>
+						<a className='nav-link' href='#'>
+							About
+						</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
+};
+
+export default Nav;
